d7c8d06d9f307654af20f975b86883a775aa6c21
Elizabeth Stein <purplegumdropz@gmail.com>
2026-02-26 09:57:16 -0500

    Fix webpack output filename to match HTML reference

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: 'index.js',
     },
     module: {
         rules: [
